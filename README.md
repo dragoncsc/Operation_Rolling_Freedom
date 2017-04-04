483f768e25c12da297302ad98e8ddfb6b4558a0a
Forrest Davis <fld2111@columbia.edu>
2017-04-04 02:04:47 -0400

    members returns list of members and info

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 Freedom and all its allies
+
+Date April 4, 2017:
+        Created script members.py that calls propublica api and gets list of all current senate members. Then a list consisting of 101 entries, one for each member is created. Each entry is itself a list of the following attributes in the given order: First Name, Last Name, State, National Representative (True/False), Senate or House member, party affliation
+
+
--- members.py
+++ members.py
@@ -23,4 +23,21 @@ for key in raw_dict:
     if key == "results":
         data_dict = raw_dict[key][0]
 
-for key in data_dict = 
+members_list = data_dict["members"]
+
+#Initalize cause im a basic betch
+members = [None]*len(members_list)
+
+for x in xrange(len(members_list)):
+    member = members_list[x]
+    f_name = member["first_name"]
+    l_name = member["last_name"]
+    state = member["state"]
+    national_rep = True
+    house = chamber
+    party = member["party"]
+    
+    info = [f_name, l_name, state, national_rep, house, party]
+    members[x] = info
+
+print members
