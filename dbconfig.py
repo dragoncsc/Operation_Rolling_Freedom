488f47b7e0f80a7f150977eead3a0343121bb15c
dragoncsc <sd2920@columbia.edu>
2017-04-04 02:03:51 -0400

    basic format for adding reps, need to merge with spi calls to propublica

--- dbconfig.py
+++ dbconfig.py
@@ -2,14 +2,42 @@ from peewee import *
 
 db = SqliteDatabase('congress.db')
 
-class Rep(Model):
+class Rep(BaseModel):
     firstName = CharField()
     lastName = CharField()
 	state = CharField()
     national_rep = BooleanField()
 	house = CharField()
 	party = CharField()
-	
 
     class Meta:
+		order_by = ('username', )
         database = db # This model uses the "people.db" database.
+
+
+
+def create_tables():
+	db.connect()
+	db.create_tables([Rep], True)
+	db.close()
+
+def add_rep( rep_dict ):
+	try:
+		with db.transaction():
+			rep = Rep.create(
+				firstname = rep_dict['fname'],
+				lastname = rep_dict['lname'],
+				state = rep_dict['state'],
+				national_rep = True,
+				house = rep_dict['house'],
+				party = rep_dict['party']
+			)
+	except IntegrityError:
+		print "something went horribly wrong"
+			
+
+
+
+
+
+
