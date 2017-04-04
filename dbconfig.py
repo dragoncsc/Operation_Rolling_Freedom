cd90599ed8ea1f308c671a27a2dee85138c9c0d0
dragoncsc <sd2920@columbia.edu>
2017-04-04 13:44:45 -0400

    added method for inserting into db

--- dbconfig.py
+++ dbconfig.py
@@ -1,19 +1,30 @@
+'''
+Shell of dbconfig file, currently only db model for reps have been defined
+TODO: 
+1) add more relevant fields for Rep, 
+2) create Bill Model
+3) create relationship mapping between Rep and Bill
+
+'''
+
 from peewee import *
 
 db = SqliteDatabase('congress.db')
 
-class Rep(BaseModel):
-    firstName = CharField()
-    lastName = CharField()
+class BaseModel(Model):
+	class Meta:
+		database = db
+
+class Rep( BaseModel ):
+	firstName = CharField()
+	lastName = CharField()
 	state = CharField()
-    national_rep = BooleanField()
+	national_rep = BooleanField()
 	house = CharField()
 	party = CharField()
 
-    class Meta:
-		order_by = ('username', )
-        database = db # This model uses the "people.db" database.
-
+	class Meta:
+		order_by = ('firstName', )
 
 
 def create_tables():
@@ -21,17 +32,18 @@ def create_tables():
 	db.create_tables([Rep], True)
 	db.close()
 
-def add_rep( rep_dict ):
+def add_rep( rep_list ):
 	try:
 		with db.transaction():
-			rep = Rep.create(
-				firstname = rep_dict['fname'],
-				lastname = rep_dict['lname'],
-				state = rep_dict['state'],
-				national_rep = True,
-				house = rep_dict['house'],
-				party = rep_dict['party']
-			)
+			rep = Rep.insert(
+				firstName = rep_list[0],
+				lastName = rep_list[1],
+				state = rep_list[2],
+				national_rep = rep_list[3],
+				house = rep_list[4],
+				party = rep_list[5]
+			).execute()
+			return rep
 	except IntegrityError:
 		print "something went horribly wrong"
 			
--- members.py
+++ members.py
@@ -1,43 +1,85 @@
+'''
+
+Quick and dirty script to grab members from propublica and dump
+basic info into a local db
+'''
 import requests
 from apikey import propublicakey
+from dbconfig import *
 
-base_url = "https://api.propublica.org/congress/v1/"
+def get_reps_info():
+    base_url = "https://api.propublica.org/congress/v1/"
 
-congress_number = "115"
+    congress_number = "115"
 
-chamber = "senate"
+    chamber = "senate"
 
-all_mem_url = congress_number +"/"+chamber+"/members.json"
+    all_mem_url = congress_number +"/"+chamber+"/members.json"
 
-header_name = "X-API-Key"
+    header_name = "X-API-Key"
 
-header = {header_name : propublicakey}
+    header = {header_name : propublicakey}
 
-r = requests.get(base_url+all_mem_url, headers=header)
+    r = requests.get(base_url+all_mem_url, headers=header)
+    return r
 
-raw_dict = r.json()
 
-data_dict = None
+def jsonify_reps( r ):
+    raw_dict = r.json()
 
-for key in raw_dict:
-    if key == "results":
-        data_dict = raw_dict[key][0]
+    data_dict = None
 
-members_list = data_dict["members"]
+    for key in raw_dict:
+        if key == "results":
+            data_dict = raw_dict[key][0]
 
-#Initalize cause im a basic betch
-members = [None]*len(members_list)
+    members_list = data_dict["members"]
 
-for x in xrange(len(members_list)):
-    member = members_list[x]
-    f_name = member["first_name"]
-    l_name = member["last_name"]
-    state = member["state"]
-    national_rep = True
-    house = chamber
-    party = member["party"]
-    
-    info = [f_name, l_name, state, national_rep, house, party]
-    members[x] = info
+    #Initalize cause im a basic betch
+    members = [None]*len(members_list)
 
-print members
+    chamber = "senate"
+
+    for x in xrange(len(members_list)):
+        member = members_list[x]
+        f_name = member["first_name"]
+        l_name = member["last_name"]
+        state = member["state"]
+        national_rep = True
+        house = chamber
+        party = member["party"]
+        
+        info = [f_name, l_name, state, national_rep, house, party]
+        members[x] = info
+
+    return members
+
+def load_members_db():
+    members = jsonify_reps( get_reps_info() )
+
+    create_tables()
+
+    for member in members:
+        add_rep( member )
+        print member[0]
+        print member[1]
+        print member[2]
+        print member[3]
+        print member[4]
+        print member[5]
+        print '\n\n'
+
+load_members_db()
+
+
+
+
+
+
+
+
+
+
+
+
+
